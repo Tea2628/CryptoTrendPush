c42370d686561b3fb60010db0783ea6904356f9e
Tea2628 <taom46126@gmail.com>
2025-06-26 00:15:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-from coin_list import coin_symbols
-from analyze import analyze_coin
-from telegram_push import send_message
 
-def main():
-    all_results = []
-
-    for symbol in coin_symbols:
-        result = analyze_coin(symbol)
-        print(result)
-        all_results.append(result)
-
-    message = "\n".join(all_results)
-    send_message(f"📊 分析结果：\n\n{message}")
-
-if __name__ == "__main__":
-    main()
