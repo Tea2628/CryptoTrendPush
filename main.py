5d6f1148b65e8efd7c3dfbb2eaa40916e3c67354
Tea2628 <taom46126@gmail.com>
2025-06-26 00:15:51 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from coin_list import coin_symbols
+from analyze import analyze_coin
+from telegram_push import send_message
+from datetime import datetime
 
+def main():
+    all_results = []
+
+    for symbol in coin_symbols:
+        result = analyze_coin(symbol)
+        print(result)
+        all_results.append(result)
+
+    now = datetime.now().strftime("%Y-%m-%d %H:%M")
+    message = f"📈 数据分析推送\n时间：{now}\n\n" + "\n".join(all_results)
+    send_message(message)
+
+if __name__ == "__main__":
+    main()
